939d82eb52f9b362e18d0f10e26c023728576741
Mariana Garcia <maridgarcia045@gmail.com>
2023-12-15 14:45:08 -0300

    changing route

--- controller/controller.js
+++ controller/controller.js
@@ -3,7 +3,7 @@ const infoRouter = express.Router();
 const service = require("../service/service");
 
 
-infoRouter.get("/all", async (_req, res) => {
+infoRouter.get("/", async (_req, res) => {
     const allInfo = await service.getAllInfo();
 
     res.status(200).json(allInfo);
